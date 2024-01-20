a3368cdfb9be7b9bb73e43c04a605f195ba5e76d
Bernardo Andrade <berdfandrade@gmail.com>
2024-01-20 20:45:47 -0300

    --

---
+++ contained.c
@@ -0,0 +1,11 @@
+/* -*- compile-command: "gcc -Wall -Werror -lcap -lseccomp contained.c -o contained" -*- */
+/* This code is licensed under the GPLv3. You can find its text here:
+   https://www.gnu.org/licenses/gpl-3.0.en.html */
+#include <stdio.h>
+
+
+int main () {
+    printf("Hello world!\n");
+
+};
+
