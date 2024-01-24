d9c87ae0bcb354031441bb8f40a1c65306d63961
Bernardo Andrade <berdfandrade@gmail.com>
2024-01-24 19:43:27 -0300

    --

--- contained.c
+++ contained.c
@@ -1,10 +1,47 @@
 /* -*- compile-command: "gcc -Wall -Werror -lcap -lseccomp contained.c -o contained" -*- */
 /* This code is licensed under the GPLv3. You can find its text here:
    https://www.gnu.org/licenses/gpl-3.0.en.html */
+#define GNU_SOURCE
+#include <errno.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <pwd.h>
+#include <sched.h>
+#include <seccomp.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/capability.h>
+#include <sys/mount.h>
+#include <sys/prctl.h>
+#include <sys/resource.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <linux/capability.h>
+#include <linux/limits.h>
 
-
-int main () {
-    printf("Hello world!\n");
+struct child_config {
+  int argc; 
+  uid_t uid; 
+  int fd; 
+  char *hostname; 
+  char **argv;
+  char *mount_dir;
 };
 
+<<capabilities>>
+
+<<mounts>>
+
+<<syscalls>>
+
+<<resources>>
+
+<<child>>
+
+<<choose-hostname>>
