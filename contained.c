9f33e1bb24cc6961ba8b4ebfd51551bf62699ea5
Bernardo Andrade <berdfandrade@gmail.com>
2024-01-24 21:27:51 -0300

    --

--- contained.c
+++ contained.c
@@ -1,6 +1,7 @@
 /* -*- compile-command: "gcc -Wall -Werror -lcap -lseccomp contained.c -o contained" -*- */
 /* This code is licensed under the GPLv3. You can find its text here:
    https://www.gnu.org/licenses/gpl-3.0.en.html */
+
 #define GNU_SOURCE
 #include <errno.h>
 #include <fcntl.h>
@@ -25,23 +26,54 @@
 #include <linux/capability.h>
 #include <linux/limits.h>
 
-struct child_config {
-  int argc; 
-  uid_t uid; 
-  int fd; 
-  char *hostname; 
+struct child_config
+{
+  int argc;
+  uid_t uid;
+  int fd;
+  char *hostname;
   char **argv;
   char *mount_dir;
 };
 
-<<capabilities>>
+// << capabilities >>
 
-<<mounts>>
+//     << mounts >>
 
-<<syscalls>>
+//     << syscalls >>
 
-<<resources>>
+//     << resources >>
 
-<<child>>
+//     << child >>
 
-<<choose-hostname>>
+//     << choose - hostname >>
+
+int main()
+{
+  struct child_config config = {0};
+  int err = 0;
+  int option = 0;
+  int sockets[2] = {0};
+  pid_t child_pid = 0;
+  int last_optind = 0;
+	while ((option = getopt(argc, argv, "c:m:u:"))) {
+		switch (option) {
+		case 'c':
+			config.argc = argc - last_optind - 1;
+			config.argv = &argv[argc - config.argc];
+			goto finish_options;
+		case 'm':
+			config.mount_dir = optarg;
+			break;
+		case 'u':
+			if (sscanf(optarg, "%d", &config.uid) != 1) {
+				fprintf(stderr, "badly-formatted uid: %s\n", optarg);
+				goto usage;
+			}
+			break;
+		default:
+			goto usage;
+		}
+		last_optind = optind;
+	}
+}
